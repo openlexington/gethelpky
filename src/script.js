eb14b2495b81de99bda0da0a016b4b1e08b14939
Erik Schwartz <erik@erikschwartz.net>
2016-06-18 19:28:17 -0400

    Enable google analytics

--- src/script.js
+++ src/script.js
@@ -52,6 +52,7 @@ define(function(require) {
   require('ui/scroll').attachTo(document);
   require('ui/project').attachTo(document);
   require('data/facet').attachTo(document);
+  require('data/analytics').attachTo(document);
   require('data/search').attachTo(document);
   require('data/typeahead').attachTo(document);
   require('data/geojson').attachTo(document);
